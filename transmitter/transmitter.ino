740e6d2639184cff203175cb14d671a01c3c34e5
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2024-01-08 17:01:12 +0700

    IR

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,13 +1,13 @@
 #include <IRremote.hpp>
-int pin=7, value=0;
+int pin = 7, value = 0;
 boolean i = 0;
 void setup()
 {
   Serial.begin(9600);
   IrSender.begin(3);
-  pinMode(pin,INPUT);
+  pinMode(pin, INPUT);
 }
-void loop() 
+void loop()
 {
   int val = digitalRead(pin);
   Serial.println(val);
