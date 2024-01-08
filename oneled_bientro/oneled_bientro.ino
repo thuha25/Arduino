8b43178ab1717bb820a3ca211e2894503781b518
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2024-01-08 17:08:29 +0700

    led_tro

--- oneled_bientro/oneled_bientro.ino
+++ oneled_bientro/oneled_bientro.ino
@@ -1,26 +1,28 @@
-//điều khiển tốc độ nháy led = biến trở 
+// điều khiển tốc độ nháy led = biến trở
 int led = 9;
-void setup(){
-  pinMode(led,OUTPUT);
+void setup()
+{
+  pinMode(led, OUTPUT);
 }
-void loop(){
+void loop()
+{
   int inputvalue = analogRead(A0);
-  int x = map(inputvalue,0,1023,100,2000);
-  digitalWrite(led,HIGH);
+  int x = map(inputvalue, 0, 1023, 100, 2000);
+  digitalWrite(led, HIGH);
   delay(x);
-  digitalWrite(led,LOW);
+  digitalWrite(led, LOW);
   delay(x);
 }
 
-//điều khiển sáng tối led bằng biến trở
-// int led = 9;  //chân sung
-// void setup(){
-//   pinMode(led,OUTPUT);
-// }
-// void loop(){
-//   int inputval = analogRead(A0);
-//   int x = map(inputval,0,1023,0,255);
-//   analogWrite(led,x);
-// }
+// điều khiển sáng tối led bằng biến trở
+//  int led = 9;  //chân sung
+//  void setup(){
+//    pinMode(led,OUTPUT);
+//  }
+//  void loop(){
+//    int inputval = analogRead(A0);
+//    int x = map(inputval,0,1023,0,255);
+//    analogWrite(led,x);
+//  }
 
-//chân âm chân A0 chân 5v
+// chân âm chân A0 chân 5v
