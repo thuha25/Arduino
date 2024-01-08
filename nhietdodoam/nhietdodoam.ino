cb1eb22b74698ad23650974fc0e124ee38db85e7
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2024-01-08 17:07:25 +0700

    dht

--- nhietdodoam/nhietdodoam.ino
+++ nhietdodoam/nhietdodoam.ino
@@ -1,12 +1,13 @@
 // Gọi thư viện DHT11
-#include "DHT.h"       
-#define DHTPin 4     
+#include "DHT.h"
+#define DHTPin 4
 #define DHTType DHT11
 #define BellPin 11
- 
+
 DHT HT(DHTPin, DHTType);
- 
-void setup() {
+
+void setup()
+{
   pinMode(BellPin, OUTPUT);
   Serial.begin(9600);
   HT.begin();
@@ -14,18 +15,22 @@ void setup() {
 }
 
 int x = 0;
- 
-void loop() {
-  float humidity = HT.readHumidity(); 
-  float temperaturec = HT.readTemperature( );
+
+void loop()
+{
+  float humidity = HT.readHumidity();
+  float temperaturec = HT.readTemperature();
   float temperatureF = HT.readTemperature(true);
 
   x = (x + 1) % 180;
   int val = 1000 * sin(x);
 
-  if(humidity > 70 && temperaturec > 25) {
+  if (humidity > 70 && temperaturec > 25)
+  {
     tone(BellPin, val);
-  } else {
+  }
+  else
+  {
     noTone(BellPin);
   }
 
