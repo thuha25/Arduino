9fc58456c96ff6c7154eaef22cd801bfbca61762
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2024-01-08 17:13:06 +0700

    hongngoai

--- hongngoai/hongngoai.ino
+++ hongngoai/hongngoai.ino
@@ -1,19 +1,22 @@
 const int InfraredPin = 4;
-const int LED=10;
+const int LED = 10;
 void setup()
 {
   pinMode(LED, OUTPUT);
-  pinMode(InfraredPin,INPUT);
+  pinMode(InfraredPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop()
 {
-  if(digitalRead(InfraredPin)==HIGH){
-    digitalWrite(LED,HIGH);
+  if (digitalRead(InfraredPin) == HIGH)
+  {
+    digitalWrite(LED, HIGH);
     Serial.println("anything move in!");
-  }else{
-    digitalWrite(LED,LOW);
+  }
+  else
+  {
+    digitalWrite(LED, LOW);
     Serial.println("this is nothing!");
   }
   delay(50);
