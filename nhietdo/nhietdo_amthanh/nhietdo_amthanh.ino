fe0dd36f20b47569debe1de423c210ef516ab74e
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2024-01-08 17:07:51 +0700

    dht

--- nhietdo/nhietdo_amthanh/nhietdo_amthanh.ino
+++ nhietdo/nhietdo_amthanh/nhietdo_amthanh.ino
@@ -1,27 +1,33 @@
 const int ledPin = 13;
 const int buzzlePin = 4;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  pinMode(ledPin,OUTPUT);
+  pinMode(ledPin, OUTPUT);
   pinMode(buzzlePin, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   int val = analogRead(A1);
   float temp = val * 500.0 / 1023.0;
   Serial.println(temp);
-  if(temp > 27.0) {
-    for(int x = 0; x < 180; x++) {
+  if (temp > 27.0)
+  {
+    for (int x = 0; x < 180; x++)
+    {
       float val = (sin(x * (3.1412 / 180.0)));
       float toneVal = 2000 + int(val * 1000);
       tone(buzzlePin, toneVal);
       delay(2);
     }
     digitalWrite(ledPin, HIGH);
-  }else{
+  }
+  else
+  {
     noTone(buzzlePin);
-    digitalWrite(ledPin, LOW); 
+    digitalWrite(ledPin, LOW);
   }
   delay(1000);
 }
