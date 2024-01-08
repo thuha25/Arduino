bfe520f202fcd5423e4bc8e626f5ee72ab3cf5ef
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2024-01-08 17:03:21 +0700

    qtro

--- quangtro/quangtro.ino
+++ quangtro/quangtro.ino
@@ -1,19 +1,23 @@
 int sensorPin = A0;
 int led = 13;
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  pinMode(led,OUTPUT);
+  pinMode(led, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   int sensorValue = analogRead(sensorPin);
   Serial.println(sensorValue);
   delay(500);
-  if(sensorValue<50){
-    digitalWrite(led,HIGH);
+  if (sensorValue < 50)
+  {
+    digitalWrite(led, HIGH);
   }
-  else{
-    digitalWrite(led,LOW);
+  else
+  {
+    digitalWrite(led, LOW);
   }
   delay(300);
 }
