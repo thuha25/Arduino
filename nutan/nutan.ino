6ac0aa1498b38102cd7c6229a991ebb66adae4f2
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2024-01-08 17:07:03 +0700

    nut

--- nutan/nutan.ino
+++ nutan/nutan.ino
@@ -1,4 +1,4 @@
-int led=13, pin=7, value=0;
+int led = 13, pin = 7, value = 0;
 // void setup(){
 //   pinMode(led,OUTPUT);
 //   pinMode(pin,INPUT);
@@ -9,23 +9,28 @@ int led=13, pin=7, value=0;
 //   digitalWrite(led,value);
 // }
 
-
-//nhấn nhả nút
+// nhấn nhả nút
 boolean i = 0;
 void setup()
 {
   pinMode(pin, INPUT);
-  pinMode(led, OUTPUT);  
+  pinMode(led, OUTPUT);
 }
-void loop(){
-  if(digitalRead(pin) == HIGH){
+void loop()
+{
+  if (digitalRead(pin) == HIGH)
+  {
     i = !i;
-    while(digitalRead(pin) == HIGH);
+    while (digitalRead(pin) == HIGH)
+      ;
     delay(200);
   }
-  if(i==1){
-    digitalWrite(led,HIGH);
-  }else{
-    digitalWrite(led,LOW);
+  if (i == 1)
+  {
+    digitalWrite(led, HIGH);
+  }
+  else
+  {
+    digitalWrite(led, LOW);
   }
 }
