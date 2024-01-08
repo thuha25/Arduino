33e5e46c970e942554b8be48c551778565e4789d
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2024-01-08 16:48:29 +0700

    receiver

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -3,17 +3,21 @@
 #define LED 13
 
 bool LED_VAL = false;
-void setup() {
-  pinMode(LED,OUTPUT);
+void setup()
+{
+  pinMode(LED, OUTPUT);
   IrReceiver.begin(IR_RECEIVE_PIN);
 }
 
-void loop() {
-  if(IrReceiver.decode()){
+void loop()
+{
+  if (IrReceiver.decode())
+  {
     uint32_t val = IrReceiver.decodedIRData.decodedRawData;
-    if(val==1){
+    if (val == 1)
+    {
       LED_VAL = !LED_VAL;
-      digitalWrite(LED,LED_VAL);
+      digitalWrite(LED, LED_VAL);
     }
     IrReceiver.resume();
   }
