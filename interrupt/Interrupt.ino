548d58a8e628f2c0575afeaa69be1f047c6dd38b
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2024-01-08 17:12:26 +0700

    interupt

--- interrupt/Interrupt.ino
+++ interrupt/Interrupt.ino
@@ -1,18 +1,18 @@
 #define LED 13
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   pinMode(LED, OUTPUT);
-  attachInterrupt(0,displayMicros,FALLING);//RISING khi nhấn nút, tức là ngắt khi nhấn nút
-  attachInterrupt(1,displayMillis,FALLING);
+  attachInterrupt(0, displayMicros, FALLING); // RISING khi nhấn nút, tức là ngắt khi nhấn nút
+  attachInterrupt(1, displayMillis, FALLING);
 }
 
 void loop()
 {
-  digitalWrite(LED,HIGH);
+  digitalWrite(LED, HIGH);
   delay(500);
   digitalWrite(LED, LOW);
   delay(500);
-
 }
 void displayMicros()
 {
